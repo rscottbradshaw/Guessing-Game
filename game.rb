9f2f594aeabfcf81187691d79af98c37ee047516
rscottbradshaw <scottbradshaw@Scotts-MacBook-Pro.local>
2014-06-03 17:38:10 -0400

    initial game file

---
+++ game.rb
@@ -0,0 +1,36 @@
+class Game
+  def initialize
+    @number = rand(1..100)
+    @count = 0
+    @win = false
+  end
+
+def user_guess
+  print 'Please guess a number between 1-100.'
+  guess = gets.chomp.to_i
+  user_feedback(guess)
+end
+
+def user_feedback(guess)
+  if guess > @number
+    puts 'Sorry too high'
+    @count += 1
+  elsif guess < @number
+    puts 'Sorry too low'
+    @count += 1
+  else guess == @number
+    @win = true
+    puts 'Great guess, you got it!!'
+  end
+end
+
+def start_game
+  while @win == false && @count < 5
+  user_guess
+end
+end
+end
+
+
+my_game = Game.new
+my_game.start_game
